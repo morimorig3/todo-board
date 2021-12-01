1d0fa76776cc73aa6759fd2df5827d8faa5c2977
morimorig3 <mori.nto@outlook.jp>
2021-12-01 11:10:01 +0900

    App / index / layoutにLinterを適用

--- src/App.tsx
+++ src/App.tsx
@@ -18,5 +18,4 @@ const App: VFC = () => (
     <TodoApp />
   </Layout>
 );
-
 export default App;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,18 +4,16 @@ type Props = {
   children?: ReactNode;
 };
 
-const Layout: VFC<Props> = ({ children }) => {
-  return (
-    <div className="container mx-auto px-4 pb-10">
-      <a
-        href="https://www.morimorig3.com/"
-        className="font-righteous block text-center text-xl leading-none font-bold py-8 text-gray-100"
-      >
-        morimorig3.com
-      </a>
-      <main className="rounded-lg bg-gray-100 p-4">{children}</main>
-    </div>
-  );
-};
+const Layout: VFC<Props> = ({ children }) => (
+  <div className="container mx-auto px-4 pb-10">
+    <a
+      href="https://www.morimorig3.com/"
+      className="font-righteous block text-center text-xl leading-none font-bold py-8 text-gray-100"
+    >
+      morimorig3.com
+    </a>
+    <main className="rounded-lg bg-gray-100 p-4">{children}</main>
+  </div>
+);
 
 export default Layout;
