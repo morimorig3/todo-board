85a37f85642b23fd7a3f02fb298c144488418646
morimorig3 <mori.nto@outlook.jp>
2021-12-01 11:56:23 +0900

    hooksにLinterを適用

--- src/hooks/useInputText.ts
+++ src/hooks/useInputText.ts
@@ -2,8 +2,9 @@ import { useState } from 'react';
 
 const useInputText = () => {
   const [value, setValue] = useState('');
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
+  };
   const clearValue = () => setValue('');
 
   return { value, clearValue, handleOnChange };
