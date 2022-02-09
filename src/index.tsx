4292dd0bbceaffab8349428cf2bdb5aec95bb8bf
morimorig3 <mori.nto@outlook.jp>
2022-02-09 15:02:50 +0900

    create-react-appとtailwindcssをアップグレード

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
